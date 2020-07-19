3638cdbd1dda620bd9e4adf4a75674e40109775d
Peter-waithaka <peterwaithaka120@gmail.com>
2020-07-19 17:20:26 +0300

    added cc

--- js/scripts.js
+++ js/scripts.js
@@ -9,14 +9,26 @@ function myFunction() {
 
     var year_element = document.getElementById("year");
     var year = year_element.value;
+    var year_arr =(year).toString().split('',2).map(Number);
+    var cc = Number(year_arr.join(''));
     console.log(year)
     
-    var myRadio = document.getElementsByName("female").value;
-    
-    console.log(myRadio)  
-    
-    var myRadio = document.getElementsByName("male").value;
-    
-    console.log(myRadio)  
+  
+    console.log(year_arr)
+    console.log(cc)
+
+      
+    const rbs = document.querySelectorAll('input[name="gender"]');
+    let selectedValue;
+    for (const rb of rbs) {
+        if (rb.checked) {
+            selectedValue = rb.value;
+            break;
+        }
+    }
+    console.log(selectedValue)
 
 }
+ 
+Day_of_the_week (d) = ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) mod 7
+console.log(d)
