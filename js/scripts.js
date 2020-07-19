73f8b010f6afb742991b63f20b533fe7b57f8ac2
Peter-waithaka <peterwaithaka120@gmail.com>
2020-07-19 20:46:58 +0300

    added bckg

--- js/scripts.js
+++ js/scripts.js
@@ -1,23 +1,26 @@
 function myFunction() {
-    var day_element = document.getElementById("day");
-    var day = day_element.value;
-    console.log(day)
+    // var day_element = document.getElementById("day");
+    // var day = day_element.value;
+    // console.log(day)
 
-    var month_element = document.getElementById("month");
-    var month = month_element.value;
-    console.log(month)
+    // var month_element = document.getElementById("month");
+    // var month = month_element.value;
+    // console.log(month)
 
-    var year_element = document.getElementById("year");
-    var year = year_element.value;
-    var year_arr =(year).toString().split('',2).map(Number);
-    var cc = Number(year_arr.join(''));
-    console.log(year)
+    // var year_element = document.getElementById("year");
+    // var year = year_element.value;
+    // var year_arr =(year).toString().split('',2).map(Number);
+    // var cc = Number(year_arr.join(''));
+    // console.log(year)
     
   
-    console.log(year_arr)
-    console.log(cc)
+    // console.log(year_arr)
+    // console.log(cc)
 
-      
+    var currentDate = document.getElementById('datepicker').value;
+    alert(currentDate);
+    var n = currentDate.getDay()
+    console.log(n);
     const rbs = document.querySelectorAll('input[name="gender"]');
     let selectedValue;
     for (const rb of rbs) {
@@ -29,6 +32,7 @@ function myFunction() {
     console.log(selectedValue)
 
 }
+
  
-Day_of_the_week (d) = ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day ) mod 7
-console.log(d)
+// Day_of_the_week (d) = ( ( (CC/4) -2*CC-1) + ((5*year/4) ) + ((26*(month+1)/10)) + day )
+// console.log(d)
