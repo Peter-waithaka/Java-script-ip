074b6c4552439cdb948002e761a2e276026fe2bc
Peter-waithaka <peterwaithaka120@gmail.com>
2020-07-19 16:33:38 +0300

    first js

---
+++ js/scripts.js
@@ -0,0 +1,22 @@
+function myFunction() {
+    var day_element = document.getElementById("day");
+    var day = day_element.value;
+    console.log(day)
+
+    var month_element = document.getElementById("month");
+    var month = month_element.value;
+    console.log(month)
+
+    var year_element = document.getElementById("year");
+    var year = year_element.value;
+    console.log(year)
+    
+    var myRadio = document.getElementsByName("female").value;
+    
+    console.log(myRadio)  
+    
+    var myRadio = document.getElementsByName("male").value;
+    
+    console.log(myRadio)  
+
+}
